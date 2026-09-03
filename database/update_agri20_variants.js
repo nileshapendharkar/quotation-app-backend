790373f22628de00d7dcf828a8b3647dfa1669dc
Nilesh Pendharkar <nileshapendharkar@users.noreply.github.com>
2026-09-03 12:54:34 +0530

    Update AGRI COUPLER 10KG variants (25MM: FG-400669, 32MM: FG-400670) and packing data

---
+++ database/update_agri20_variants.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const dbPath = path.join(__dirname, 'db_data.json');
+const data = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
+
+const p = data.products.find(p => p.id === 'prod_agri_20');
+if (!p) {
+  console.error('Product prod_agri_20 not found!');
+  process.exit(1);
+}
+
+p.name = 'AGRI COUPLER 10KG';
+p.sizes = [
+  '25MM',
+  '32MM'
+];
+p.description = `AGRI COUPLER 10KG. Available sizes: ${p.sizes.join(', ')}.`;
+
+p.packSizes = {
+  '25MM': 450,
+  '32MM': 250
+};
+
+p.packings = {
+  '25MM': 450,
+  '32MM': 250
+};
+
+p.sizeProductCodes = {
+  '25MM': 'FG-400669',
+  '32MM': 'FG-400670'
+};
+
+fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+console.log('✅ Successfully updated prod_agri_20:', JSON.stringify(p, null, 2));
