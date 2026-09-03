b1669c16b3f072957d36922e71acd98583fe30d5
Nilesh Pendharkar <nileshapendharkar@users.noreply.github.com>
2026-09-03 12:31:09 +0530

    Update PN10 - TEE ISI to AGRI TEE 10KG and remove sizes/packing

---
+++ database/update_agri19.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const dbPath = path.join(__dirname, 'db_data.json');
+const data = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
+
+const p = data.products.find(p => p.id === 'prod_agri_19');
+if (!p) {
+  console.error('Product prod_agri_19 not found!');
+  process.exit(1);
+}
+
+p.name = 'AGRI TEE 10KG';
+p.description = 'AGRI TEE 10KG.';
+p.sizes = [];
+delete p.packSizes;
+delete p.sizeProductCodes;
+delete p.packings;
+delete p.packing;
+delete p.packSize;
+
+fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+console.log('✅ Successfully updated prod_agri_19:', JSON.stringify(p, null, 2));
