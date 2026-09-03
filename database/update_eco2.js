f71887dfb5e01c9fda91da602281fbeb3243a676
Nilesh Pendharkar <nileshapendharkar@users.noreply.github.com>
2026-09-03 14:39:31 +0530

    Update RINGFIT PIPE to ECO DRAINAGE PIPES RINGFIT and remove sizes/packing

---
+++ database/update_eco2.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const dbPath = path.join(__dirname, 'db_data.json');
+const data = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
+
+const p = data.products.find(p => p.id === 'prod_eco_2');
+if (!p) {
+  console.error('Product prod_eco_2 not found!');
+  process.exit(1);
+}
+
+p.name = 'ECO DRAINAGE PIPES RINGFIT';
+p.description = 'ECO DRAINAGE PIPES RINGFIT.';
+p.sizes = [];
+delete p.packSizes;
+delete p.sizeProductCodes;
+delete p.packings;
+delete p.packing;
+delete p.packSize;
+
+fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+console.log('✅ Successfully updated prod_eco_2:', JSON.stringify(p, null, 2));
